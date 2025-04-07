148ee08cc92d8c5907c8e8cd269bf646a11c9d91
mishtvinomnis <misha@madebyomnis.com>
2025-04-07 19:36:12 +0300

    suk9

--- backup-current/server/managers/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Модульные менеджеры для управления разными аспектами приложения
- */
-export * from './EventEmitter';
-export * from './SlotManager';
-export * from './CameraManager';
-export * from './ConnectionManager';
-export * from './TokenManager';
